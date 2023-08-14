c519439328ba957ccc2454b1efd2de6dc9e876a3
Naomi <naomistrall@hotmail.com>
2023-08-14 16:21:43 -0300

    desisto

--- src/controllers/poros.controllers.js
+++ src/controllers/poros.controllers.js
@@ -32,3 +32,37 @@ export async function getPoroById(req, res) {
     res.status(500).send(error.message);
   }
 }
+
+export async function GetMyPoros(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const myUser = (
+      await db.query(`SELECT * FROM sessions WHERE token=$1`, [token])
+    ).rows[0];
+
+    if (!myUser) return res.sendStatus(409);
+
+    const myPoros = (
+      await db.query(`SELECT * FROM poros WHERE "ownerId"=$1`, [myUser.userId])
+    ).rows;
+
+    res.status(200).send(myPoros);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function TurnPoroDisabled(req, res) {
+  const { id } = req.params;
+
+  try {
+    await db.query(`UPDATE poros SET available='false' WHERE id = $1;`, [id]);
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routes/poros.routes.js
+++ src/routes/poros.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { getPoroById, getPoros } from "../controllers/poros.controllers.js";
+import {
+  GetMyPoros,
+  TurnPoroDisabled,
+  getPoroById,
+  getPoros,
+} from "../controllers/poros.controllers.js";
 
 const porosRoutes = Router();
 
 porosRoutes.get("/poros", getPoros);
 porosRoutes.get("/poros/:id", getPoroById);
+porosRoutes.get("/my/poros", GetMyPoros);
+porosRoutes.get("/my/poros/:id", TurnPoroDisabled);
 export default porosRoutes;
